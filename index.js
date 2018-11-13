505abfa49329edca4963dae19196199f4b173a97
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-11-13 18:17:13 -0500

    Reorganized code, includes new Button

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-const feedButton = document.getElementById('feed-button')
-
 const BASE_URL = 'http://localhost:3000/'
+const feedingContainer = document.getElementById('feed-button-container')
+
 
 fetch(BASE_URL + 'feedings/last_meal')
 .then(response => response.json())
 .then(lastMeal => {
-    const feedingContainer = document.getElementById('feed-button-container')
-    feedingContainer.innerHTML = `<h6>Fish Last Fed: ${lastMeal}</h6>
-    <a class ="button is-large is-primary" id="feed-button">Feed Fish</a>`
-    // let h6 = document.createElement('h6')
-    // h6.innerText = `Fish Last Fed: ${lastMeal}`
-    // feedingContainer.appendChild(h6)
+  renderLandingPage(lastMeal)
+  const feedButton = document.getElementById('feed-button')
+  feedButton.addEventListener('click', createFeeding)
+  const fishRoomButton = document.getElementById('fish-room-button')
+  fishRoomButton.addEventListener('click', renderFishroomPage)
+
 })
+.catch(error => {
+  feedingContainer.innerHTML = "<h6> Connection to Server failed! </h6>"
+})
+
+
+
+function renderLandingPage(lastMeal) {
+  feedingContainer.innerHTML = `
+    <h6>Fish Last Fed: ${lastMeal}</h6>
+    <a class ="button is-large is-primary" id="feed-button">Feed Fish</a>
+    <a class ="button is-large is-primary" id="fish-room-button">Fish Room</a>
+  `
+}
+
+function createFeeding(event){
+  fetch(BASE_URL + 'feedings', {
+    method: 'POST'
+  })
+  .then(res=>{
+    RenderFishFed()
+  })
+  .catch(error => {debugger})
+
+}
+
+function renderFishroomPage(){
+
+}
