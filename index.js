14b3cb1847b365ced1eb16bb97a99f2400c4d77b
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-11-14 16:53:58 -0500

    Added Section 3! View page for tank

--- index.js
+++ index.js
@@ -2,45 +2,42 @@ const BASE_URL = 'http://localhost:3000/'
 const feedingContainer = document.getElementById('feed-button-container')
 const section1 = document.getElementById('section1')
 const section2 = document.getElementById('section2')
+const section3 = document.getElementById('section3')
 
 fetch(BASE_URL + 'feedings/last_meal')
 .then(response => response.json())
 .then(lastMeal => {
-  renderLandingPage(lastMeal)
+    renderLandingPage(lastMeal)
 
-  // for section1
-  const feedButton = document.getElementById('feed-button')
-  feedButton.addEventListener('click', createFeeding)
+    // for section1
+    const feedButton = document.getElementById('feed-button')
+    feedButton.addEventListener('click', createFeeding)
 
 
-  // for section2
-  section2.addEventListener('click', event =>{
-    const target = event.target.id
-    if (target === 'left'){
-      renderFishroomPage(target)
-      // section2.innerHTML =
-    } else if (target === 'right'){
-      renderFishroomPage(target)
-      // section2.innerHTML =
-    } else if (target === 'back'){
-      renderFishroomPage(target)
-      // section2.innerHTML =
-    }
-  })
+    // for section2
+    section2.addEventListener('click', event =>{
+        const target = event.target.id
+        console.log(target)
 
+        if (target === 'left' || target === 'right' || target === 'back'){
+            renderFishroomPage(target)
+        } else if (parseInt(target)) {
+            renderTankshowPage(target)
+        }
+    })
 })
 .catch(error => {
-  feedingContainer.innerHTML = "<h6> Connection to Server failed! </h6>"
+    feedingContainer.innerHTML = "<h6> Connection to Server failed! </h6>"
 })
 
 
 
 function renderLandingPage(lastMeal) {
-  feedingContainer.innerHTML = `
-    <h6 class ="subtitle is-4"><strong>Fish Last Fed:</strong> ${lastMeal}</h6>
-    <a class ="button is-large is-primary" id="feed-button">Feed Fish</a><br><br>
-    <a href = "#section2" class ="button is-large is-primary" id="fish-room-button">Fish Room</a>
-  `
+      feedingContainer.innerHTML = `
+        <h6 class ="subtitle is-4"><strong>Fish Last Fed:</strong> ${lastMeal}</h6>
+        <a class ="button is-large is-primary" id="feed-button">Feed Fish</a><br><br>
+        <a href = "#section2" class ="button is-large is-primary" id="fish-room-button">Fish Room</a>
+      `
 }
 
 function createFeeding(event){
@@ -58,7 +55,6 @@ function renderFishroomPage(sectionName){
   fetch(`${BASE_URL}tanks/section/${sectionName}`)
   .then(res => res.json())
   .then(tanks => {
-
     section2.innerHTML = `
       <div class="tile is-ancestor">
         <div class="tile is-parent" >
@@ -69,12 +65,28 @@ function renderFishroomPage(sectionName){
 
       section2.firstElementChild.firstElementChild.innerHTML += `
 
-          <div class="tile is-child" id= "left">
-            <h2 class= "title button"> ${tank.name} </h2>
+          <div class="tile is-child">
+            <h2 class= "title button" id= "${tank.id}"> ${tank.name} </h2>
           </div>
       `
     })
   })
+}
 
+function renderTankshowPage(id){
+    fetch(`${BASE_URL}tanks/${id}`)
+    .then(res => res.json())
+    .then(tank => {
 
+        let fishList = ""
+        tank.fish.forEach( fish => {
+            fishList += `<li>${fish.name}</li>`
+        })
+
+        section3.innerHTML =`
+            Section: ${tank.section} <br>
+            Name: ${tank.name} <br>
+            Fish: ${fishList}
+        `
+    })
 }
